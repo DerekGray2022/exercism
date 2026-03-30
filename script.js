5b15104578696145db86bf0bd495531221f4dcc2
DerekGray2022 <derekgray99@yahoo.com>
2026-03-30 21:42:38 +0100

    Simple Cipher | Test 3 CORRECTED

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // a-z charCodes => 97 - 122
 const utf8encoder = new TextEncoder();
-// const text = "z";
-// const res = utf8encoder.encode(text)[0];
-
-const alpha = "abcdefghijklmnopqrstuvwxyz".split("");
 
 class Cipher {
   constructor(cipherKey) {
@@ -14,6 +10,7 @@ class Cipher {
   }
 
   encode(message) {
+    this.encoded = "";
     message && (this.message = message);
     let y = 0;
 
@@ -40,6 +37,7 @@ class Cipher {
   }
 
   decode(keyCode) {
+    this.decoded = "";
     let y = 0;
     for (let x = 0; x < keyCode.length; x++) {
       // Check for cipherKey overflow
@@ -77,56 +75,55 @@ class Cipher {
 // -------------    Random key cipher   --------------
 
 // can encode
-const cipher0 = new Cipher();
-const res0 = cipher0.encode("aaaaaaaaaa"); //  .toEqual(cipher.key.substring(0, 10));
+const cipher = new Cipher();
+const res0 = cipher.encode("aaaaaaaaaa"); //  .toEqual(cipher.key.substring(0, 10));
 
 // can decode
-const res1 = cipher0.decode(cipher0.key.substring(0, 10)); //  .toEqual('aaaaaaaaaa');
+const res1 = cipher.decode(cipher.key.substring(0, 10)); //  .toEqual('aaaaaaaaaa');
 
 // is reversible
-const cipher1 = new Cipher();
 const plaintext = "abcdefghij";
-const enc = cipher1.encode(plaintext)
-const res2 = cipher1.decode(enc); //  .toEqual(plaintext);
+const enc = cipher.encode(plaintext)
+const res2 = cipher.decode(enc); //  .toEqual(plaintext);
 
-// key is made only of lowercase letters
-const res3 = Boolean(cipher1.key.match(/^[a-z]+$/)); //  .toMatch(/^[a-z]+$/);
+// // key is made only of lowercase letters
+// const res3 = Boolean(cipher1.key.match(/^[a-z]+$/)); //  .toMatch(/^[a-z]+$/);
 
-// // -------------    Substitution cipher   --------------
+// // // -------------    Substitution cipher   --------------
 
-// can encode
-const key = "abcdefghij";
-const cipher2 = new Cipher(key);
-const res4 = cipher2.encode("aaaaaaaaaa"); //  .toEqual('abcdefghij');
+// // can encode
+// const key = "abcdefghij";
+// const cipher2 = new Cipher(key);
+// const res4 = cipher2.encode("aaaaaaaaaa"); //  .toEqual('abcdefghij');
 
-// can decode
-const res5 = cipher2.decode("abcdefghij"); //  .toEqual('aaaaaaaaaa');
+// // can decode
+// const res5 = cipher2.decode("abcdefghij"); //  .toEqual('aaaaaaaaaa');
 
-// is reversible
-const cipher3 = new Cipher(key);
-const res6 = cipher3.decode(cipher3.encode("abcdefghij")); //  .toEqual('abcdefghij');
+// // is reversible
+// const cipher3 = new Cipher(key);
+// const res6 = cipher3.decode(cipher3.encode("abcdefghij")); //  .toEqual('abcdefghij');
 
-// can double shift encode
-const res7 = new Cipher("iamapandabear").encode("iamapandabear")    //  .toEqual("qayaeaagaciai");
+// // can double shift encode
+// const res7 = new Cipher("iamapandabear").encode("iamapandabear")    //  .toEqual("qayaeaagaciai");
 
-// can wrap on encode
-const cipher4 = new Cipher(key);
-const res8 = cipher4.encode('zzzzzzzzzz')   //  .toEqual('zabcdefghi');
+// // can wrap on encode
+// const cipher4 = new Cipher(key);
+// const res8 = cipher4.encode('zzzzzzzzzz')   //  .toEqual('zabcdefghi');
 
-// can wrap on decode
-const res9 = cipher4.decode('zabcdefghi')   //  .toEqual('zzzzzzzzzz');
+// // can wrap on decode
+// const res9 = cipher4.decode('zabcdefghi')   //  .toEqual('zzzzzzzzzz');
 
-// can encode messages longer than the key
-const res10 = new Cipher('abc').encode('iamapandabear') //  .toEqual('iboaqcnecbfcr');
+// // can encode messages longer than the key
+// const res10 = new Cipher('abc').encode('iamapandabear') //  .toEqual('iboaqcnecbfcr');
 
 console.log(res0);
 console.log(res1);
 console.log(res2);
-console.log(res3);
-console.log(res4);
-console.log(res5);
-console.log(res6);
-console.log(res7);
-console.log(res8);
-console.log(res9);
-console.log(res10);
+// console.log(res3);
+// console.log(res4);
+// console.log(res5);
+// console.log(res6);
+// console.log(res7);
+// console.log(res8);
+// console.log(res9);
+// console.log(res10);
