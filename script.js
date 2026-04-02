fef3dfb007f97f701f38543a8906f1c51fbfab88
DerekGray2022 <derekgray99@yahoo.com>
2026-04-02 13:11:46 +0100

    Reverse String| passed

--- script.js
+++ script.js
@@ -1,58 +1,79 @@
 // go
 
-// A leap year (in the Gregorian calendar) occurs:
+const reverseString = (str) => {
+  let res = "";
+  if (str) {
+    res = str.split("").reverse();
 
-//     In every year that is evenly divisible by 4.
-//     Unless the year is evenly divisible by 100, in which case it's only a leap year if the year is also evenly divisible by 400.
-
-// Some examples:
-
-//     1997 was not a leap year as it's not divisible by 4.
-//     1900 was not a leap year as it's not divisible by 400.
-//     2000 was a leap year!
-
-const isLeap = (year) => {
-  if (year % 100 === 0) {
-    return year % 400 === 0;
+    let index = res.indexOf("̈");
+    if (index === -1) res.indexOf("ี");
+    if (index !== -1) {
+    const symbol = res.slice(index, index + 1);
+    res.splice(index, 1);
+    res.splice(index + 1, 0, symbol[0])
+    }
+    res = res.reduce((acc, curr) => {
+      return (acc += curr);
+    });
   }
-  return year % 4 === 0;
+  return res;
 };
 
-//  "A leap year"
-// "year not divisible by 4 in common year"
-const res0 = isLeap(2015); //  .toBe(false);
+//  "empty string"
+let expected = "";
+let actual = reverseString("");
+const res0 = actual === expected; //  .toEqual(expected);
 
-// "year divisible by 2, not divisible by 4 in common year"
-const res1 = isLeap(1970); //  .toBe(false);
+// //  "a word"
+// expected = "tobor";
+// actual = reverseString("robot");
+// const res1 = actual === expected; //  .toEqual(expected);
 
-// "year divisible by 4, not divisible by 100 in leap year"
-const res2 = isLeap(1996); //  .toBe(true);
+// //  "a capitalized word"
+// expected = "nemaR";
+// actual = reverseString("Ramen");
+// const res2 = actual === expected; //  .toEqual(expected);
 
-// "year divisible by 4 and 5 is still a leap year"
-const res3 = isLeap(1960); //  .toBe(true);
+// //  "a sentence with punctuation"
+// expected = "!yrgnuh ma I";
+// actual = reverseString("I am hungry!");
+// const res3 = actual === expected; //  .toEqual(expected);
 
-// "year divisible by 100, not divisible by 400 in common year"
-const res4 = isLeap(2100); //  .toBe(false);
+// //  "a palindrome"
+// expected = "racecar";
+// actual = reverseString("racecar");
+// const res4 = actual === expected; //  .toEqual(expected);
 
-//  "year divisible by 100 but not by 3 is still not a leap year"
-const res5 = isLeap(1900); //  .toBe(false);
+// //  "an even-sized word", () => {
+// expected = "reward";
+// actual = reverseString("drawer");
+// const res5 = actual === expected; //  .toEqual(expected);
 
-//  "year divisible by 400 in leap year"
-const res6 = isLeap(2000); //  .toBe(true);
+// //  "wide characters"
+// expected = "猫子";
+// actual = reverseString("子猫");
+// const res6 = actual === expected; //  .toEqual(expected);
 
-//  "year divisible by 400 but not by 125 is still a leap year"
-const res7 = isLeap(2400); //  .toBe(true);
+// // ------------------------------------------
 
-//  "year divisible by 200, not divisible by 400 in common year"
-const res8 = isLeap(1800); //  .toBe(false);
+// //  "grapheme cluster with pre-combined form"
+//   expected = "dnatsnehctsrüW";
+//   actual = reverseString("Würstchenstand");
+//   const res7 = actual === expected //  .toEqual(expected);
 
-console.log(res0);
-console.log(res1);
-console.log(res2);
-console.log(res3);
-console.log(res4);
-console.log(res5);
-console.log(res6);
-console.log(res7);
+
+// //  "grapheme clusters"
+//   expected = "มรกแรปโนยขีเผู้";
+//   actual = reverseString("ผู้เขียนโปรแกรม");
+//   const res8 = actual === expected //  .toEqual(expected);
+
+
+// console.log(res0);
+// console.log(res1);
+// console.log(res2);
+// console.log(res3);
+// console.log(res4);
+// console.log(res5);
+// console.log(res6);
+// console.log(res7);
 console.log(res8);
-
