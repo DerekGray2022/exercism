dffda5a74dd12173d19d4032d9be68c587a6cec0
DerekGray2022 <derekgray99@yahoo.com>
2026-04-03 14:32:41 +0100

    Triangle | passed

--- playground.js
+++ playground.js
@@ -1,23 +1,19 @@
+// go
 
+class Pizza {
+  constructor(size) {
+    this._size = size;
+    this._crust = "original";
+  }
 
+  getCrust() {
+    return this._crust;
+  }
 
-// Source - https://stackoverflow.com/a/16155417
-// Posted by fernandosavio, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-04-01, License - CC BY-SA 4.0
-
-function dec2bin(dec) {
-  return (dec >>> 0).toString(2);
+  setCrust(crust) {
+    this._crust = crust;
+  }
 }
 
-console.log(dec2bin(24)); // 1
-// console.log(dec2bin(-1)); // 11111111111111111111111111111111
-// console.log(dec2bin(256)); // 100000000
-// console.log(dec2bin(-256)); // 11111111111111111111111100000000
-
-
-
-
-
-
-
+const pizza1 = new Pizza("large", "stuffed");
 
--- script.js
+++ script.js
@@ -1,79 +1,168 @@
 // go
 
-const reverseString = (str) => {
-  let res = "";
-  if (str) {
-    res = str.split("").reverse();
+// a + b ≥ c
+// b + c ≥ a
+// c + a ≥ b
 
-    let index = res.indexOf("̈");
-    if (index === -1) res.indexOf("ี");
-    if (index !== -1) {
-    const symbol = res.slice(index, index + 1);
-    res.splice(index, 1);
-    res.splice(index + 1, 0, symbol[0])
-    }
-    res = res.reduce((acc, curr) => {
-      return (acc += curr);
-    });
+class Triangle {
+  constructor(...sides) {
+    const [a, b, c] = [...sides];
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.one = this.a + this.b >= this.c;
+    this.two = this.b + this.c >= this.a;
+    this.three = this.c + this.a >= this.b;
+    this.zero = this.a === 0 || this.b === 0 || this.c === 0;
+    this.output = false;
+
+    this.isTriangle = () => {
+      if (!this.one || !this.two || !this.three || this.zero) {
+        this.output = false;
+      }
+      return this.output;
+    };
   }
-  return res;
-};
 
-//  "empty string"
-let expected = "";
-let actual = reverseString("");
-const res0 = actual === expected; //  .toEqual(expected);
+  get isEquilateral() {
+    if (this.a === this.b && this.b === this.c) {
+      this.output = true;
+    } 
+    this.isTriangle();
+    return this.output;
+  }
 
-// //  "a word"
-// expected = "tobor";
-// actual = reverseString("robot");
-// const res1 = actual === expected; //  .toEqual(expected);
+  get isIsosceles() {
+    if (this.a === this.b || this.b === this.c || this.c === this.a) {
+      this.output = true;
+    }
+    this.isTriangle();
+    return this.output;
+  }
 
-// //  "a capitalized word"
-// expected = "nemaR";
-// actual = reverseString("Ramen");
-// const res2 = actual === expected; //  .toEqual(expected);
+  get isScalene() {
+    if (this.a !== this.b && this.b !== this.c && this.c !== this.a) {
+      this.output = true;
+    }
+    this.isTriangle();
+    return this.output;
+  }
+}
 
-// //  "a sentence with punctuation"
-// expected = "!yrgnuh ma I";
-// actual = reverseString("I am hungry!");
-// const res3 = actual === expected; //  .toEqual(expected);
+// equilateral triangle
 
-// //  "a palindrome"
-// expected = "racecar";
-// actual = reverseString("racecar");
-// const res4 = actual === expected; //  .toEqual(expected);
+// all sides are equal
+const triangle0 = new Triangle(2, 2, 2);
+const res0 = triangle0.isEquilateral; //  .toBe(true);
 
-// //  "an even-sized word", () => {
-// expected = "reward";
-// actual = reverseString("drawer");
-// const res5 = actual === expected; //  .toEqual(expected);
+// any side is unequal
+const triangle1 = new Triangle(2, 3, 2);
+const res1 = triangle1.isEquilateral; //  .toBe(false);
 
-// //  "wide characters"
-// expected = "猫子";
-// actual = reverseString("子猫");
-// const res6 = actual === expected; //  .toEqual(expected);
+// no sides are equal
+const triangle2 = new Triangle(5, 4, 6);
+const res2 = triangle2.isEquilateral; //  .toBe(false);
 
-// // ------------------------------------------
+// all zero sides is not a triangle
+const triangle3 = new Triangle(0, 0, 0);
+const res3 = triangle3.isEquilateral; //  .toBe(false);
 
-// //  "grapheme cluster with pre-combined form"
-//   expected = "dnatsnehctsrüW";
-//   actual = reverseString("Würstchenstand");
-//   const res7 = actual === expected //  .toEqual(expected);
+// sides may be floats
+const triangle4 = new Triangle(0.5, 0.5, 0.5);
+const res4 = triangle4.isEquilateral; //  .toBe(true);
 
+// --------------------------------------------------
 
-// //  "grapheme clusters"
-//   expected = "มรกแรปโนยขีเผู้";
-//   actual = reverseString("ผู้เขียนโปรแกรม");
-//   const res8 = actual === expected //  .toEqual(expected);
+// isosceles triangle
 
+// // last two sides are equal
+// const triangle5 = new Triangle(3, 4, 4);
+// const res5 = triangle5.isIsosceles; //  .toBe(true);
 
-// console.log(res0);
-// console.log(res1);
-// console.log(res2);
-// console.log(res3);
-// console.log(res4);
+// // first two sides are equal
+// const triangle6 = new Triangle(4, 4, 3);
+// const res6 = triangle6.isIsosceles; //  .toBe(true);
+
+// // first and last sides are equal
+// const triangle7 = new Triangle(4, 3, 4);
+// const res7 = triangle7.isIsosceles; //  .toBe(true);
+
+// // equilateral triangles are also isosceles
+// const triangle8 = new Triangle(4, 4, 4);
+// const res8 = triangle8.isIsosceles; //  .toBe(true);
+
+// // no sides are equal
+// const triangle9 = new Triangle(2, 3, 4);
+// const res9 = triangle9.isIsosceles; //  .toBe(false);
+
+// // first triangle inequality violation
+// const triangle10 = new Triangle(1, 1, 3);
+// const res10 = triangle10.isIsosceles; //  .toBe(false);
+
+// // second triangle inequality violation
+// const triangle11 = new Triangle(1, 3, 1);
+// const res11 = triangle11.isIsosceles; //  .toBe(false);
+
+// // third triangle inequality violation
+// const triangle12 = new Triangle(3, 1, 1);
+// const res12 = triangle12.isIsosceles; //  .toBe(false);
+
+// // sides may be floats
+// const triangle13 = new Triangle(0.5, 0.4, 0.5);
+// const res13 = triangle13.isIsosceles; //  .toBe(true);
+
+// -----------------------------------------
+
+// scalene triangle
+
+// // no sides are equal
+// const triangle14 = new Triangle(5, 4, 6);
+// const res14 = triangle14.isScalene; //  .toBe(true);
+
+// // all sides are equal
+// const triangle15 = new Triangle(4, 4, 4);
+// const res15 = triangle15.isScalene; //  .toBe(false);
+
+// // first and second sides are equal
+// const triangle16 = new Triangle(4, 4, 3);
+// const res16 = triangle16.isScalene; //  .toBe(false);
+
+// // first and third sides are equal
+// const triangle17 = new Triangle(3, 4, 3);
+// const res17 = triangle17.isScalene; //  .toBe(false);
+
+// // second and third sides are equal
+// const triangle18 = new Triangle(4, 3, 3);
+// const res18 = triangle18.isScalene; //  .toBe(false);
+
+// // may not violate triangle inequality
+// const triangle19 = new Triangle(7, 3, 2);
+// const res19 = triangle19.isScalene; //  .toBe(false);
+
+// // sides may be floats
+// const triangle20 = new Triangle(0.5, 0.4, 0.6);
+// const res20 = triangle20.isScalene; //  .toBe(true);
+
+console.log(res0);
+console.log(res1);
+console.log(res2);
+console.log(res3);
+console.log(res4);
+// ----------------------------------------------
 // console.log(res5);
 // console.log(res6);
 // console.log(res7);
-console.log(res8);
+// console.log(res8);
+// console.log(res9);
+// console.log(res10);
+// console.log(res11);
+// console.log(res12);
+// console.log(res13);
+// // ----------------------------------------------
+// console.log(res14);
+// console.log(res15);
+// console.log(res16);
+// console.log(res17);
+// console.log(res18);
+// console.log(res19);
+// console.log(res20);
