96cbd657bb85d6e94f38cbed4ade8eec5defbb0c
DerekGray2022 <derekgray99@yahoo.com>
2026-04-06 18:06:06 +0100

    Hamming | Passed

--- script.js
+++ script.js
@@ -1,93 +1,69 @@
 // go
 
-const transform = (old) => {
-  let outObj = {};
-  let res = Object.keys(old);
+const compute = (strandA, strandB) => {
+  let count = 0;
 
-  //#region Collect letters, pair them with value & assign to outObj
-  res.forEach((key) => {
-    old[key].forEach((letter) => {
-      letter = letter.toLowerCase();
-      const out = { [letter]: Number(key) };
-      Object.assign(outObj, out);
-    });
-  });
-  //#endregion
+  if (strandA.length !== strandB.length) {
+    throw new Error("strands must be of equal length");
+  }
 
-  //#region Sort outObj by keys
-  let output = Object.keys(outObj)
-    .sort()
-    .reduce((obj, key) => {
-      obj[key] = outObj[key];
-      return obj;
-    }, {});
-    //#endregion
+  for (let x = 0; x < strandA.length; x++) {
+    (strandA[x] !== strandB[x]) && count++;
+  }
 
-  return output;
+  return count;
 };
 
-// -------------------------------------------------
+// ----------------------------------------
 
-// Transform legacy to new
+// Hamming
 
-// single letter
-const old0 = { 1: ["A"] };
-const expected0 = { a: 1 };
-const res0 = transform(old0); //  .toEqual(expected0);
+// empty strands
+const res0 = compute("", ""); //  .toEqual(0);
 
-// single score with multiple letters
-const old1 = { 1: ["A", "E", "I", "O", "U"] };
-const expected1 = { a: 1, e: 1, i: 1, o: 1, u: 1 };
-const res1 = transform(old1); // .toEqual(expected1);
+// single letter identical strands
+const res1 = compute("A", "A"); //  .toEqual(0);
 
-// multiple scores with multiple letters
-const old2 = { 1: ["A", "E"], 2: ["D", "G"] };
-const expected2 = { a: 1, e: 1, d: 2, g: 2 };
-const res2 = transform(old2); //  .toEqual(expected2);
+// single letter different strands
+const res2 = compute("G", "T"); //  .toEqual(1);
 
-// multiple scores with differing numbers of letters
-const old3 = {
-  1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-  2: ["D", "G"],
-  3: ["B", "C", "M", "P"],
-  4: ["F", "H", "V", "W", "Y"],
-  5: ["K"],
-  8: ["J", "X"],
-  10: ["Q", "Z"],
-};
-const expected3 = {
-  a: 1,
-  b: 3,
-  c: 3,
-  d: 2,
-  e: 1,
-  f: 4,
-  g: 2,
-  h: 4,
-  i: 1,
-  j: 8,
-  k: 5,
-  l: 1,
-  m: 3,
-  n: 1,
-  o: 1,
-  p: 3,
-  q: 10,
-  r: 1,
-  s: 1,
-  t: 1,
-  u: 1,
-  v: 4,
-  w: 4,
-  x: 8,
-  y: 4,
-  z: 10,
-};
-const res3 = transform(old3); //  .toEqual(expected3);
+// long identical strands
+const res3 = compute("GGACTGAAATCTG", "GGACTGAAATCTG"); //  .toEqual(0);
 
-// ------------------------------------------------
+// long different strands
+const res4 = compute("GGACGGATTCTG", "AGGACGGATTCT"); //  .toEqual(9);
+
+// disallow first strand longer
+const res5 = () => compute("AATG", "AAA"); /*  .toThrow(
+  new Error("strands must be of equal length"),
+);  */
+
+// disallow second strand longer
+const res6 = () => compute("ATA", "AGTG"); /*  .toThrow(
+  new Error("strands must be of equal length"),
+); */
+
+// disallow empty first strand
+const res7 = () => compute("", "G"); /*  .toThrow(
+  new Error("strands must be of equal length"),
+);  */
+
+// disallow empty second strand
+const res8 = () => compute("G", "") /*  .toThrow(
+  new Error("strands must be of equal length"),
+);  */
+
+// ----------------------------------------
 
 console.log(res0);
 console.log(res1);
 console.log(res2);
 console.log(res3);
+console.log(res4);
+// console.log(res5());
+// console.log(res6());
+// console.log(res7());
+// console.log(res8());
+
+
+
