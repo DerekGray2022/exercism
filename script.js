d076774551d6c48c6e0fb00fd325eef7a164c9da
DerekGray2022 <derekgray99@yahoo.com>
2026-04-09 21:55:21 +0100

    Difference of Squares | Passed

--- script.js
+++ script.js
@@ -1,198 +1,95 @@
 // go
 
-class BankAccount {
-  constructor() {
-    this.status = false;
-    this._balance = 0;
+class Squares {
+  constructor(square) {
+    this._square = square;
   }
 
-  open() {
-    if (this.status) throw new ValueError();
-    this.status = true;
+  get sumOfSquares() {
+    let _sumOfSquare = 0;
+    for (let x = 0; x <= this._square; x++) {
+      let z = Math.pow(x, 2);
+      _sumOfSquare += z;
+    }
+
+    return _sumOfSquare;
   }
 
-  close() {
-    if (!this.status) throw new ValueError();
-    this.status = false;
-    this._balance = 0;
+  get squareOfSum() {
+    let _squareOfSum = 0;
+    for (let x = 0; x <= this._square; x++) {
+      _squareOfSum += x;
+    }
+
+    return Math.pow(_squareOfSum, 2);
   }
 
-  deposit(deposit) {
-    if (!this.status || deposit < 0) throw new ValueError();
-    this._balance += deposit;
-  }
-
-  withdraw(deposit) {
-    if (!this.status || deposit > this._balance || deposit < 0) throw new ValueError();
-    this._balance -= deposit;
-  }
-
-  get balance() {
-    if (!this.status) throw new ValueError();
-    return this._balance;
+  get difference() {
+    return  this.squareOfSum - this.sumOfSquares;
   }
 }
 
-export class ValueError extends Error {
-  constructor() {
-    super("Bank account error");
-  }
-}
+// ------------------------------------------------
 
-// ----------------------------------------------------
+//  difference-of-squares
+const squares1 = new Squares(1);
+const squares5 = new Squares(5);
+const squares100 = new Squares(100);
 
-//  Bank Account
+//  Square the sum of the numbers up to the given number
 
-// //  newly opened account has zero balance
-// const account0 = new BankAccount();
-// account0.open();
-// const res0 = account0.balance; //  .toEqual(0);
+//  "square of sum 1", () => {
+const res0 = squares1.squareOfSum; //  .toBe(1);
 
-// //  Single deposit
-//   const account1 = new BankAccount();
-//   account1.deposit(100);
-//   const res1 = account1.balance  //  .toEqual(100);
+//  square of sum 5
+const res1 = squares5.squareOfSum; //  .toBe(225);
 
-// //  Multiple deposits
-//   const account2 = new BankAccount();
-//   account2.open();
-//   account2.deposit(100);
-//   account2.deposit(50);
-//   const res2 = account2.balance  //  .toEqual(150);
+//  square of sum 100
+const res2 = squares100.squareOfSum; //  .toBe(25502500);
 
-// //  Withdraw once
-//   const account3 = new BankAccount();
-//   account3.open();
-//   account3.deposit(100);
-//   account3.withdraw(50);
-//   const res3 = account3.balance  //  .toEqual(50);
+// ------------------------------------------------
 
-// //  Withdraw twice
-//   const account4 = new BankAccount();
-//   account4.open();
-//   account4.deposit(100);
-//   account4.withdraw(20);
-//   account4.withdraw(80);
-//   const res4 = account4.balance  //  .toEqual(0);
+//  Sum the squares of the numbers up to the given number
 
-// //  Can do multiple operations sequentially
-//   const account5 = new BankAccount();
-//   account5.open();
-//   account5.deposit(100);
-//   account5.deposit(110);
-//   account5.withdraw(200);
-//   account5.deposit(60);
-//   account5.withdraw(50);
-//   const res5 = account5.balance  //  .toEqual(20);
+  //  "sum of squares 1
+    const res3 = squares1.sumOfSquares  //  .toBe(1);
 
-// //  Cannot check balance of closed account
-//   const account6 = new BankAccount();
-//   account6.open();
-//   account6.close();
-//   const res6 = () => account6.balance  //  .toThrow(ValueError);
+  //  sum of squares 5
+    const res4 = squares5.sumOfSquares  //  .toBe(55);
 
-// //  Cannot deposit into closed account
-//   const account7 = new BankAccount();
-//   account7.open();
-//   account7.close();
-//   const res7 = () => {account7.deposit(50);} //  .toThrow(ValueError);
+  //  sum of squares 100
+    const res5 = squares100.sumOfSquares  //  .toBe(338350);
 
-// //  Cannot deposit into unopened account
-//   const account8 = new BankAccount();
-//   const res8 = () => {account8.deposit(50);} //  .toThrow(ValueError);
+// ------------------------------------------------
 
-// //  Cannot withdraw from closed account
-//   const account9 = new BankAccount();
-//   account9.open();
-//   account9.close();
-//   const res9 = () => {account9.withdraw(50);}  //  .toThrow(ValueError);
 
-// //  Cannot close an account that was not opened
-//   const account10 = new BankAccount();
-//   const res10 = () => {account10.close();} //  .toThrow(ValueError);
+//  "Subtract sum of squares from square of sums
 
-// //  "Cannot open an already opened account
-//   const account11 = new BankAccount();
-//   account11.open();
-//   const res11 = () => {account11.open();}  //  .toThrow(ValueError);
+  //  difference of squares 1
+    const res6 = squares1.difference  //  .toBe(0);
 
-// //  Reopened account does not retain balance
-//   const account12 = new BankAccount();
-//   account12.open();
-//   account12.deposit(50);
-//   account12.close();
-//   account12.open();
-//   const res12 = account12.balance  //  .toEqual(0);
+  //  difference of squares 5
+  const res7 = squares5.difference  //  .toBe(170);
 
-// //  Cannot withdraw more than deposited
-//   const account13 = new BankAccount();
-//   account13.open();
-//   account13.deposit(25);
-//   const res13 = () => {account13.withdraw(50);} //  .toThrow(ValueError);
+  //  difference of squares 100
+    const res8 = squares100.difference  //  .toBe(25164150);
 
-// //  Cannot withdraw negative
-//   const account14 = new BankAccount();
-//   account14.open();
-//   account14.deposit(100);
-//   const res14 = () => {account14.withdraw(-50);} //  .toThrow(ValueError);
+// ------------------------------------------------
 
-// //  Cannot deposit negative
-//   const account15 = new BankAccount();
-//   account15.open();
-//   const res15 = () => {account15.deposit(-50);}  //  .toThrow(ValueError);
 
-// //  Can handle concurrent transactions
-//   const account16 = new BankAccount();
-//   let res16;
-//   account16.open();
-//   account16.deposit(1000);
-//   for (let i = 0; i < 10; i++) {
-//     await adjustBalanceConcurrently(account16);
-//     res16 = account16.balance  //  .toEqual(1000);
-//   }
+console.log(res0);
+console.log(res1);
+console.log(res2);
 
-// function adjustBalanceConcurrently(account) {
-//   const random = () => Math.floor(Math.random() * 10);
-//   const tasks = Array.from(
-//     { length: 1000 },
-//     () =>
-//       new Promise((resolve) => {
-//         try {
-//           account.deposit(5);
-//           setTimeout(() => {
-//             account.withdraw(5);
-//             resolve();
-//           }, random());
-//         } catch (e) {
-//           throw new Error(`Exception should not be thrown: ${e.message}`);
-//         }
-//       }),
-//   );
-//   return Promise.all(tasks);
-// }
+// ------------------------------------------------
 
-//  Changing balance directly throws error
-const account17 = new BankAccount();
-  account17.open();
-  const res17 = () => {account17.balance = 100;} //  .toThrow(Error);};
+console.log(res3);
+console.log(res4);
+console.log(res5);
 
-// ---------------------------------------
+// ------------------------------------------------
 
-// console.log(res0);
-// console.log(res1);
-// console.log(res2);
-// console.log(res3);
-// console.log(res4);
-// console.log(res5);
-// console.log(res6());
-// console.log(res7());
-// console.log(res8());
-// console.log(res9());
-// console.log(res10());
-// console.log(res11());
-// console.log(res12);
-// console.log(res13());
-// console.log(res14());
-// console.log(res15());
-// console.log(res16);
-console.log(res17());
+console.log(res6);
+console.log(res7);
+console.log(res8);
+
