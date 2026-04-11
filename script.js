2ab88ad41b65c268e16632cb3cc0d1516bebffd3
DerekGray2022 <derekgray99@yahoo.com>
2026-04-11 19:30:14 +0100

    Luhn | Passed

--- script.js
+++ script.js
@@ -1,95 +1,121 @@
 // go
 
-class Squares {
-  constructor(square) {
-    this._square = square;
+const valid = (str) => {
+  let total = 0;
+  // let regEx = /[\d]/g;
+  // let holder = [];
+
+  if (/[^0-9 ]/.test(str) || str.length < 2)  return false;
+
+  str = Array.from(str.trim().replaceAll(" ", ""));
+  str.reverse();
+
+  // if (str.length < 2) return false;
+
+  for (let x = 0; x < str.length; x++) {
+    str[x] = Number(str[x]);
+
+    if (!str[x]) continue;
+    if (x % 2 === 1) str[x] *= 2;
+    if (str[x] > 9) str[x] -= 9;
+
+    total += str[x];
   }
 
-  get sumOfSquares() {
-    let _sumOfSquare = 0;
-    for (let x = 0; x <= this._square; x++) {
-      let z = Math.pow(x, 2);
-      _sumOfSquare += z;
-    }
+  return total % 10 === 0 ? true : false;
+};
 
-    return _sumOfSquare;
-  }
+// -------------------------------------------
 
-  get squareOfSum() {
-    let _squareOfSum = 0;
-    for (let x = 0; x <= this._square; x++) {
-      _squareOfSum += x;
-    }
+//  Luhn
 
-    return Math.pow(_squareOfSum, 2);
-  }
+// //  single digit strings can not be valid
+// const res0 = valid("1"); //  .toEqual(false);
 
-  get difference() {
-    return  this.squareOfSum - this.sumOfSquares;
-  }
-}
+// //  a single zero is invalid
+// const res1 = valid("0"); //  .toEqual(false);
 
-// ------------------------------------------------
+// //  a simple valid SIN that remains valid if reversed
+// const res2 = valid("059"); //  .toEqual(true);
 
-//  difference-of-squares
-const squares1 = new Squares(1);
-const squares5 = new Squares(5);
-const squares100 = new Squares(100);
+// //  a simple valid SIN that becomes invalid if reversed
+// const res3 = valid("59"); //  .toEqual(true);
 
-//  Square the sum of the numbers up to the given number
+// //  a valid Canadian SIN
+// const res4 = valid("055 444 285"); //  .toEqual(true);
 
-//  "square of sum 1", () => {
-const res0 = squares1.squareOfSum; //  .toBe(1);
+// //  "invalid Canadian SIN
+// const res5 = valid("055 444 286"); //  .toEqual(false);
 
-//  square of sum 5
-const res1 = squares5.squareOfSum; //  .toBe(225);
+// //  invalid credit card
+// const res6 = valid("8273 1232 7352 0569"); //  .toEqual(false);
 
-//  square of sum 100
-const res2 = squares100.squareOfSum; //  .toBe(25502500);
+// //  invalid long number with an even remainder
+// const res7 = valid("1 2345 6789 1234 5678 9012"); //  .toEqual(false);
 
-// ------------------------------------------------
+// //  invalid long number with a remainder divisible by 5
+// const res8 = valid("1 2345 6789 1234 5678 9013"); //  .toEqual(false);
 
-//  Sum the squares of the numbers up to the given number
+// //  valid number with an even number of digits
+// const res9 = valid("095 245 88"); //  .toEqual(true);
 
-  //  "sum of squares 1
-    const res3 = squares1.sumOfSquares  //  .toBe(1);
+// //  valid number with an odd number of spaces
+// const res10 = valid("234 567 891 234"); //  .toEqual(true);
 
-  //  sum of squares 5
-    const res4 = squares5.sumOfSquares  //  .toBe(55);
+// //  valid strings with a non-digit added at the end invalid
+// const res11 = valid("059a"); //  .toEqual(false);
 
-  //  sum of squares 100
-    const res5 = squares100.sumOfSquares  //  .toBe(338350);
+// //  valid strings with punctuation included become invalid
+//   const res12 = valid("055-444-285") //  .toEqual(false);
 
-// ------------------------------------------------
+// //  valid strings with symbols included become invalid
+//   const res13 = valid("055# 444$ 285") //  .toEqual(false);
 
+// //  single zero with space is invalid
+//   const res14 = valid(" 0")  //  .toEqual(false);
 
-//  "Subtract sum of squares from square of sums
+// //  more than a single zero is valid
+//   const res15 = valid("0000 0")  //  .toEqual(true);
 
-  //  difference of squares 1
-    const res6 = squares1.difference  //  .toBe(0);
+// //  input digit 9 is correctly converted to output digit 9
+//   const res16 = valid("091") //  .toEqual(true);
 
-  //  difference of squares 5
-  const res7 = squares5.difference  //  .toBe(170);
+// //  very long input is valid
+//   const res17 = valid("9999999999 9999999999 9999999999 9999999999")  //  .toEqual(true);
 
-  //  difference of squares 100
-    const res8 = squares100.difference  //  .toBe(25164150);
+// //  valid luhn with an odd number of digits and non zero first digit
+//   const res18 = valid("109") //  .toEqual(true);
 
-// ------------------------------------------------
+// //  using ascii value for non-doubled non-digit isn't allowed
+//   const res19 = valid("055b 444 285")  //  .toEqual(false);
 
+//  using ascii value for doubled non-digit isn't allowed
+const res20 = valid(":9"); //  .toEqual(false);
 
-console.log(res0);
-console.log(res1);
-console.log(res2);
+//  non-numeric, non-space char in the middle with a sum that's divisible by 10 isn't allowed
+const res21 = valid("59%59"); //  .toEqual(false);
 
-// ------------------------------------------------
+// ------------------------------------------
 
-console.log(res3);
-console.log(res4);
-console.log(res5);
-
-// ------------------------------------------------
-
-console.log(res6);
-console.log(res7);
-console.log(res8);
-
+// console.log(res0);
+// console.log(res1);
+// console.log(res2);
+// console.log(res3);
+// console.log(res4);
+// console.log(res5);
+// console.log(res6);
+// console.log(res7);
+// console.log(res8);
+// console.log(res9);
+// console.log(res10);
+// console.log(res11);
+// console.log(res12);
+// console.log(res13);
+// console.log(res14);
+// console.log(res15);
+// console.log(res16)
+// console.log(res17);
+// console.log(res18);
+// console.log(res19);
+console.log(res20);
+console.log(res21);
