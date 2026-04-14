0e8c0833865a2618ec00bdd13dd7372312f44006
DerekGray2022 <derekgray99@yahoo.com>
2026-04-14 17:53:36 +0100

    High Scores | Passed

--- script.js
+++ script.js
@@ -1,67 +1,107 @@
 // go
 
-const parse = (entry) => {
-  const regEx = /[a-z']+/gi;
-  let workArr = [...entry.matchAll(regEx)];
-  let res = "";
+class HighScores {
+  constructor(scores) {
+    this._scores = scores;
+  }
 
-  workArr.forEach(ele => {
-    res += ele[0][0].toUpperCase();
-  });
+  get scores() {
+    return this._scores;
+  }
 
-  return res;
-};
+  get latest() {
+    return this._scores[this._scores.length - 1];
+  }
 
-// ---------------------------------------------
+  get personalBest() {
+    return Math.max(...this._scores);
+  }
 
-//  Acronyms are produced from
+  get personalTopThree() {
+    return [...this._scores].sort((a, b) => b - a).slice(0, 3);
+  }
+}
 
-  // basic
-  //  title cased phrases
-    const res0 = parse("Portable Network Graphics") // .toEqual("PNG");
+// -------------------------------------------
 
-  // lowercase words
-  //  other title cased phrases
-    const res1 = parse("Ruby on Rails") //  .toEqual("ROR");
+//  High Scores Test Suite
 
-  // punctuation
-  //  phrases with punctuation
-    const res2 = parse("First In, First Out") //  .toEqual("FIFO");
+  //  List of scores
+    const input0 = [30, 50, 20, 70];
+    const res0 = new HighScores(input0).scores //  .toEqual([30, 50, 20, 70]);
 
-  // all caps word
-  //  phrases with all uppercase words
-    const res3 = parse("GNU Image Manipulation Program")  //  .toEqual("GIMP");
+  //  Latest score
+    const input1 = [100, 0, 90, 30];
+    const res1 = new HighScores(input1).latest //  .toEqual(30);
 
-  // punctuation without whitespace
-  //  phrases with punctuation without whitespace
-    const res4 = parse("Complementary metal-oxide semiconductor") //  .toEqual("CMOS");
+  //  Personal best
+    const input2 = [40, 100, 70];
+    const res2 = new HighScores(input2).personalBest //  .toEqual(100);
 
-  // very long abbreviation
-  //  long phrases
-    const res5 = parse("Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me",
-      ) //  .toEqual("ROTFLSHTMDCOALM");
+// -------------------------------------------
 
-  // consecutive delimiters
-  //  phrases with consecutive delimiters
-    const res6 = parse("Something - I made up from thin air") //  .toEqual("SIMUFTA");
 
-  // apostrophes
-  //  phrases with apostrophes
-    const res7 = parse("Halley's Comet")  //  .toEqual("HC");
+  //  Top 3 scores
 
-  // underscore emphasis
-  //  phrases with underscore emphasis
-    const res8 = parse("The Road _Not_ Taken")  //  .toEqual("TRNT");
+    //  Personal top three from a list of scores
+      const input3 = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70];
+      const res3 = new HighScores(input3).personalTopThree //  .toEqual([100, 90, 70]);
 
-    // ------------------------------------------
+    //  Personal top highest to lowest
+      const input4 = [20, 10, 30];
+      const res4 = new HighScores(input4).personalTopThree //  .toEqual([30, 20, 10]);
 
-    console.log(res0);
-    console.log(res1);
-    console.log(res2);
-    console.log(res3);
-    console.log(res4);
-    console.log(res5);
-    console.log(res6);
-    console.log(res7);
-    console.log(res8);
-    
+    //  Personal top when there is a tie
+      const input5 = [40, 20, 40, 30];
+      const res5 = new HighScores(input5).personalTopThree //  .toEqual([40, 40, 30]);
+
+    //  Personal top when there are less than 3
+      const input6 = [30, 70];
+      const res6 = new HighScores(input6).personalTopThree //  .toEqual([70, 30]);
+
+    //  Personal top when there is only one
+      const input7 = [40];
+      const res7 = new HighScores(input7).personalTopThree //  .toEqual([40]);
+
+    //  Latest score after personal top scores
+      const input8 = [70, 50, 20, 30];
+      const highScores8 = new HighScores(input8);
+      highScores8.personalTopThree;
+      const res8 = highScores8.latest  //  .toEqual(30);
+
+    //  Scores after personal top scores
+      const input9 = [30, 50, 20, 70];
+      const highScores9 = new HighScores(input9);
+      highScores9.personalTopThree;
+      const res9 = highScores9.scores  //  .toEqual(input);
+
+    //  Latest score after personal best
+      const input10 = [20, 70, 15, 25, 30];
+      const highScores10 = new HighScores(input10);
+      highScores10.personalBest;
+      const res10 = highScores10.latest  //  .toEqual(30);
+
+    //  Scores after personal best
+      const input11 = [20, 70, 15, 25, 30];
+      const highScores11 = new HighScores(input11);
+      highScores11.personalBest;
+      const res11 = highScores11.scores  //  .toEqual(input);
+
+// -------------------------------------------
+
+console.log(res0);
+console.log(res1);
+console.log(res2);
+
+// -------------------------------------------
+
+
+console.log(res3);
+console.log(res4);
+console.log(res5);
+console.log(res6);
+console.log(res7);
+console.log(res8);
+console.log(res9);
+console.log(res10);
+console.log(res11);
