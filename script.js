bd6bb41d5185b466e4561ca105a878ed8e3b1324
DerekGray2022 <derekgray99@yahoo.com>
2026-04-17 00:43:31 +0100

    Largest Series Product | Passed

--- script.js
+++ script.js
@@ -1,88 +1,97 @@
 // go
 
-export class Series {
-  constructor(series) {
-    this._series = series;
-    if (this._series.length <= 0) throw new Error("series cannot be empty");
+const largestProduct = (No, bracket) => {
+  const regEx = /[^0-9]/;
+  let workArr = [];
+  let high = 0;
+
+  if (bracket > No.length) throw new Error("span must not exceed string length");
+  if (regEx.test(No)) throw new Error("digits input must only contain digits");
+  if (bracket <= 0) throw new Error("span must not be negative");
+
+  //#region Isolate each bracket
+  for (let x = 0; x <= No.length - bracket; x++) {
+    let arr = [];
+    arr.push(No.slice(x, x + bracket));
+    workArr.push(arr);
   }
+  //#endregion
 
-  slices(sliceLength) {
-    let output = [];
-
-    if (sliceLength > this._series.length) throw new Error("slice length cannot be greater than series length");
-
-    for (let x = 0; x <= this._series.length - sliceLength; x++) {
-      let working = [];
-      if (sliceLength < 0) throw new Error("slice length cannot be negative");
-      if (sliceLength === 0) throw new Error("slice length cannot be zero");
-
-      for (let y = x; y < x + sliceLength; y++) {
-        working.push(Number(this._series[y]));
-      }
-      output.push(working);
+  //#region Calculate product of each bracket
+  workArr.forEach(arr => {
+    let total = Number(arr[0][0]);
+    
+    for (let x = 1; x < arr[0].length; x++) {
+      total *= Number(arr[0][x]);
     }
 
-    return output;
-  }
-}
+    if (total > high) high = total;
+  });
+  //#endregion
 
-// -----------------------------------------------
-
-//  Series
-
-// //  slices of one from one
-// const res0 = new Series("1").slices(1); //  .toEqual([[1]]);
-
-// //  slices of one from two
-// const res1 = new Series("12").slices(1); //  .toEqual([[1],  [2]]);
-
-// //  slices of two
-// const res2 = new Series("35").slices(2); //  .toEqual([[3, 5]]);
-
-// //  slices of two overlap
-// const res3 = new Series("9142").slices(2); //  .toEqual([[9, 1], [1, 4], [4, 2],]);
-
-// // slices can include duplicates
-// const res4 = new Series("777777").slices(3); //  .toEqual([[7, 7, 7], [7, 7, 7], [7, 7, 7], [7, 7, 7],]);
-
-// //  slices of long series
-// const res5 = new Series("918493904243").slices(5); //  .toEqual([ [9, 1, 8, 4, 9], [1, 8, 4, 9, 3], [8, 4, 9, 3, 9], [4, 9, 3, 9, 0], [9, 3, 9, 0, 4], [3, 9, 0, 4, 2], [9, 0, 4, 2, 4], [0, 4, 2, 4, 3],]);
-
-// //  slice length is too large
-// const res6 = () => {
-//   new Series("12345").slices(6);
-// }; //  .toThrow(new Error("slice length cannot be greater than series length"));
-
-// //  slice length is way too large
-// const res7 = () => {
-//   new Series("12345").slices(42);
-// }; //  .toThrow(new Error("slice length cannot be greater than series length"));
-
-// //  slice length cannot be zero
-// const res8 = () => {
-//   new Series("12345").slices(0);
-// }; //  .toThrow(new Error("slice length cannot be zero"));
-
-// //  slice length cannot be negative
-// const res9 = () => {
-//   new Series("123").slices(-1);
-// }; //  .toThrow(new Error("slice length cannot be negative"));
-
-//  empty series is invalid
-const res10 = () => {
-  new Series("").slices(1);
-}; //  .toThrow(new Error("series cannot be empty"));
+  return high;
+};
 
 // ---------------------------------------
 
-// console.log(res0);
-// console.log(res1);
-// console.log(res2);
-// console.log(res3);
-// console.log(res4);
-// console.log(res5);
-// console.log(res6());
-// console.log(res7());
-// console.log(res8());
+//  Largest Series Product
+
+  //  finds the largest product if span equals length
+    const res0 = largestProduct("29", 2)  //  .toEqual(18);
+
+  //  can find the largest product of 2 with numbers in order
+    const res1 = largestProduct("0123456789", 2)  //  .toEqual(72);
+
+  //  can find the largest product of 2
+    const res2 = largestProduct("576802143", 2) //  .toEqual(48);
+
+  //  can find the largest product of 3 with numbers in order
+    const res3 = largestProduct("0123456789", 3)  //  .toEqual(504);
+
+  //  can find the largest product of 3
+    const res4 = largestProduct("1027839564", 3)  //  .toEqual(270);
+
+  //  can find the largest product of 5 with numbers in order
+    const res5 = largestProduct("0123456789", 5)  //  .toEqual(15120);
+
+  //  can get the largest product of a big number
+    const res6 = largestProduct("73167176531330624919225119674426574742355349194934", 6) //  .toEqual(23520);
+
+  //  reports zero if the only digits are zero
+    const res7 = largestProduct("0000", 2)  //  .toEqual(0);
+
+  //  reports zero if all spans include zero
+    const res8 = largestProduct("99099", 3) //  .toEqual(0);
+
+// ---------------------------------------
+
+  // //  rejects span longer than string length
+  //   const res9 = () => largestProduct("123", 4) //  .toThrow(new Error("span must not exceed string length"),);
+
+  // //  rejects empty string and nonzero span
+  //   const res10 = () => largestProduct("", 1)  //  .toThrow(new Error("span must not exceed string length"),);
+
+  // //  rejects invalid character in digits
+  //   const res11 = () => largestProduct("1234a5", 2)  //  .toThrow(new Error("digits input must only contain digits"),);
+
+  //  rejects negative span
+    const res12 = () => largestProduct("12345", -1)  //  .toThrow(new Error("span must not be negative"),);
+
+// ---------------------------------------
+
+console.log(res0);
+console.log(res1);
+console.log(res2);
+console.log(res3);
+console.log(res4);
+console.log(res5);
+console.log(res6);
+console.log(res7);
+console.log(res8);
+
+// ---------------------------------------
+
 // console.log(res9());
-console.log(res10());
+// console.log(res10());
+// console.log(res11());
+console.log(res12());
